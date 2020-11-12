c7a65a689c73a9e1cc9afb4a479df9c4ed7bc64a
akhossanX <abdelilah.khossan@gmail.com>
2020-11-12 23:49:36 +0100

    getting file permissions

--- src/ls_display.c
+++ src/ls_display.c
@@ -26,6 +26,24 @@ char    get_filetype(mode_t mode)
     return ('-');
 }
 
+char	*get_permissions(mode_t mode)
+{
+	static char	perm[10];
+
+	perm[0] = (mode & S_IRUSR) ? 'R' : '-';
+	perm[1] = (mode & S_IWUSR) ? 'W' : '-';
+	perm[2] = (mode & S_IXUSR) ? 'X' : '-';
+
+	perm[3] = (mode & S_IRGRP) ? 'R' : '-';
+	perm[4] = (mode & S_IWGRP) ? 'W' : '-';
+	perm[5] = (mode & S_IXGRP) ? 'X' : '-';
+
+	perm[6] = (mode & S_IROTH) ? 'R' : '-';
+	perm[7] = (mode & S_IWOTH) ? 'W' : '-';
+	perm[8] = (mode & S_IXOTH) ? 'X' : '-';
+	return ((char *)perm);
+}
+
 void    long_display(t_ls *ls, t_path *lst, int dir_or_files)
 {
     if (dir_or_files == DIRECTORY)
@@ -33,6 +51,7 @@ void    long_display(t_ls *ls, t_path *lst, int dir_or_files)
     while (lst != NULL)
     {
         ft_printf("%c", get_filetype(lst->st->st_mode & S_IFMT));
+		ft_printf("%s", get_permissions(lst->st->st_mode));
         ft_printf("\t%s\n", lst->name);
         lst = lst->next;
     }
